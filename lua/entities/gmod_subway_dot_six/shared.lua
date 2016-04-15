dd111ee37b53c2c481745be4422a099562e7e287
Hellss <arseni0022@mail.ru>
2016-04-15 22:53:27 +0300

    Update shared.lua

--- lua/entities/gmod_subway_dot_six/shared.lua
+++ lua/entities/gmod_subway_dot_six/shared.lua
@@ -19,14 +19,18 @@ function ENT:GetStandingArea()
 	return Vector(-450,-30,-45),Vector(380,30,-45)
 end
 
+function ENT:InitializeSounds()
+	self.BaseClass.InitializeSounds(self)
+	self.SoundNames["r1_5_close"] = {"subway_trains/drive_on3.wav","subway_trains/drive_on4.wav"}
+end
 function ENT:InitializeSystems()
-	-- Электросистема 81-705
+	-- Электросистема 81-710
 	self:LoadSystem("Electric","81_717_Electric")
-	
+
 	-- Токоприёмник
-	self:LoadSystem("TR","TR_3B")	
+	self:LoadSystem("TR","TR_3B")
 	-- Электротяговые двигатели
-	self:LoadSystem("Engines","DK_117DM")	
+	self:LoadSystem("Engines","DK_117DM")
 
 	-- Резисторы для реостата/пусковых сопротивлений
 	self:LoadSystem("KF_47A")
@@ -34,7 +38,7 @@ function ENT:InitializeSystems()
 	self:LoadSystem("KF_50A")
 	-- Ящик с предохранителями
 	self:LoadSystem("YAP_57")
-	
+
 	-- Резисторы для цепей управления
 	--self:LoadSystem("YAS_44V")
 	-- Реостатный контроллер для управления пусковыми сопротивления
@@ -46,7 +50,7 @@ function ENT:InitializeSystems()
 	-- Контроллер резервного управления
 	self:LoadSystem("KRU")
 
-	
+
 	-- Ящики с реле и контакторами
 	self:LoadSystem("LK_755A")
 	self:LoadSystem("YAR_13A")
@@ -55,19 +59,22 @@ function ENT:InitializeSystems()
 	self:LoadSystem("YAK_37E")
 	self:LoadSystem("YAS_44V")
 	self:LoadSystem("YARD_2")
-	self:LoadSystem("PR_14X_Panels")	
+	self:LoadSystem("PR_14X_Panels")
 
-	-- Пневмосистема 81-705
+	-- Пневмосистема 81-710
 	self:LoadSystem("Pneumatic","81_717_Pneumatic")
-	-- Панель управления 81-705
+	-- Панель управления 81-710
 	self:LoadSystem("Panel","81_717_Panel")
 	-- Everything else
 	self:LoadSystem("Battery")
 	self:LoadSystem("PowerSupply","BPSN")
 	self:LoadSystem("DURA")
 	self:LoadSystem("ALS_ARS")
-	self:LoadSystem("PAKSD_DOOR","Relay","Switch")
-	self:LoadSystem("PAKSD_VUD","Relay","Switch")
+	self:LoadSystem("ADoorDisable","Relay")
+	self:LoadSystem("ASNP31","Relay","Switch")
+	self:LoadSystem("ASNP32","Relay","Switch")
 	self:LoadSystem("Horn")
+
 	self:LoadSystem("Announcer")
+	self:LoadSystem("ASNP")
 end
